cbe0e1aea8a2a40c21931ffdfef58a23de7223f8
Iványi Tamás <ivatam88@gmail.com>
2026-02-17 18:33:03 +0100

    text printing function

---
+++ text_writh.py
@@ -0,0 +1,33 @@
+def szoveg_kiiras(szo, ABC):
+    for sorok in range(0, len(ABC[szo[0]])):
+        for karakterek in szo:
+            for oszlopok in range(0, len(ABC[karakterek][sorok])):
+                print(ABC[karakterek][sorok][oszlopok], end= " ")
+            print("  ", end="")
+        print()
+
+hatter = input("Miből legyen a háttér? ")
+tinta = input("Miből legyen a betű? ")
+szo = input("Mi legyen a szó? ")
+
+ABC = {
+    "a":[[hatter, hatter, hatter, tinta, tinta, hatter, hatter, hatter],
+         [hatter, hatter, tinta, tinta, tinta, tinta, hatter, hatter],
+         [hatter, tinta, tinta, hatter, hatter, tinta, tinta, hatter],
+         [tinta, tinta, hatter, hatter, hatter, hatter, tinta, tinta],
+         [tinta, tinta, tinta, tinta, tinta, tinta, tinta, tinta],
+         [tinta, tinta, tinta, tinta, tinta, tinta, tinta, tinta],
+         [tinta, tinta, hatter, hatter, hatter, hatter, tinta, tinta],
+         [tinta, tinta, hatter, hatter, hatter, hatter, tinta, tinta]],
+
+    "z":[[tinta, tinta, tinta, tinta, tinta, tinta, tinta, tinta],
+         [tinta, tinta, tinta, tinta, tinta, tinta, tinta, tinta],
+         [hatter, hatter, hatter, hatter, hatter, tinta, tinta, tinta],
+         [hatter, hatter, hatter, hatter, tinta, tinta, tinta, hatter],
+         [hatter, hatter, tinta, tinta, tinta, hatter, hatter, hatter],
+         [tinta, tinta, tinta, hatter, hatter, hatter, hatter, hatter],
+         [tinta, tinta, tinta, tinta, tinta, tinta, tinta, tinta],
+         [tinta, tinta, tinta, tinta, tinta, tinta, tinta, tinta]]
+}
+
+szoveg_kiiras(szo, ABC)
